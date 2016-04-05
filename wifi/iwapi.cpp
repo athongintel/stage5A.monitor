439ce3c4b366b49f9cb3537453a3ec18b76aeee8
tom <dangxuanthong@gmail.com>
2016-04-05 17:01:16 +0200

    mesure method

--- wifi/iwapi.cpp
+++ wifi/iwapi.cpp
@@ -1,19 +1,91 @@
 #include "iwapi.h"
 
-Iwapi::Iwapi(){
+Iwapi::Iwapi() throw (){
 	//check for iw version
 	const std::string result = Utils::exec("iw --version");
-	std::cout<<result;
+	
 	std::smatch match;
-	std::regex rgx("iw version ([0-9]+[.][0-9])+");
+	std::regex rgx("iw version\\s([0-9]+[.][0-9])+");
 
     if (std::regex_search(result.begin(), result.end(), match, rgx)){
-		for (unsigned i=0; i<match.size(); ++i)
-    std::cout << "match #" << i << ": " << match[i] << std::endl;
+		this->version = match[1];
+	}
+	else{
+		throw "Error: iw not found on this computer!";
 	}
 	
 }
 
 std::vector<std::string> Iwapi::getWlanInterfaceNames(){
+	const std::string result = Utils::exec("iw dev");
+	std::vector<std::string> interfaces;	
 	
+	std::smatch match;
+	std::regex rgx("Interface\\s(wlan[0-9]+)");
+	
+	std::string::const_iterator textIterator = result.begin();
+	
+	while (regex_search(textIterator, result.end(), match, rgx)){
+		int count = match.size();
+		//for (int i=0; i<count; i++){		
+			interfaces.push_back(match[1]);
+		//}
+	   textIterator = match[0].second;
+	}
+	return interfaces;
 }
+
+
+std::vector<struct Network> Iwapi::scanNetworks(std::string  devName){
+	const std::string result = Utils::exec("iw " + devName + " scan");
+	std::vector<struct Network> networks;	
+	
+	std::smatch match;
+	std::regex rgx("BSS\\s((?:[0-9|a-f]{2}:){5}(?:[0-9|a-f]{2}))[\\S\\s]+?(?:signal:)\\s(-[0-9]+.[0-9]+)[\\S\\s]+?(?:SSID:)\\s(.*)[\\S\\s]+?(?=RSN:|\\bBSS)(?:RSN:[\\S\\s]+?(?:Group cipher:\\s(.+)))?");
+	
+	std::string::const_iterator textIterator = result.begin();
+	
+	while (regex_search(textIterator, result.end(), match, rgx)){
+		int count = match.size();
+		//for (int i=1; i<count; i++){
+			/* output from match:
+				1: bss id
+				2: signal strength in dBm
+				3: ssid
+				4: encrption method
+			*/
+			
+			//find existed network
+			struct Network* net;
+			bool found=false;
+			for  (int i=0;i<networks.size();i++){
+				net = &networks[i];
+				if (net->ssid.compare(match[3])==0){
+					found = true;
+					break;
+				}
+			}
+			
+			if (!found){
+				std::cout<<"Network "<<match[3]<<" not found"<<std::endl;
+				net = new Network();
+				net->ssid = match[3];	
+				net->aps = {};
+				networks.push_back(*net);				
+			}
+			else{
+				std::cout<<"Network "<<match[3]<<" found"<<std::endl;
+			}
+			std::cout<<"adding bss: "<<match[1]<<std::endl;
+			struct BSS* bss = new BSS();
+			bss->id = match[1];
+			std::string signal = match[2];
+			bss->signal = atof(signal.c_str());
+			bss->encryption = match[4];
+			net->aps.push_back(*bss);			
+		//}
+	   textIterator = match[3].second;
+	}
+	return networks;
+}
+
--- wifi/iwapi.h
+++ wifi/iwapi.h
@@ -6,11 +6,26 @@
 #include <vector>
 #include "utils.h"
 
+struct BSS{
+	std::string id;
+	double signal;
+	std::string encryption;
+};
+
+struct Network{
+	std::string ssid;
+	std::vector<struct BSS> aps;
+	
+};
+
 class Iwapi{
 	
+	std::string version;
+	
 	public:
-		Iwapi();	
+		Iwapi() throw ();	
 		std::vector<std::string> getWlanInterfaceNames();
+		std::vector<struct Network> scanNetworks(std::string  devName);
 
 };
 
