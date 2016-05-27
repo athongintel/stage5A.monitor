681d8b4381b5e11427b88e064f981d3098710100
root <root@localhost.localdomain>
2016-05-27 16:31:03 +0200

    - change main.cpp to display only interface

--- wifi/netlink/main.cpp
+++ wifi/netlink/main.cpp
@@ -14,7 +14,7 @@ void test_connect(){
 	int apcount;
 	for (auto &i : interfaces){
 		cout<<i->getName()<<" "<<i->getDisplayableMacAddress()<<endl;
-		cout<<"I'm performing a full scan..."<<endl;
+		/*cout<<"I'm performing a full scan..."<<endl;
 		networks = i->fullNetworkScan();	
 		for (auto &net : networks){
 			cout<<networkcount<<": "<<(net->SSID.empty()? "Hidden network" : net->SSID)<<endl;
@@ -23,7 +23,7 @@ void test_connect(){
 			/*if (strcmp(net->SSID, "(Aix*Marseille universite")==0){
 				cout<<"Trying to connect to "<<net->SSID<<" access point..."<<endl;				
 			}
-			bool connect = false;*/
+			bool connect = false;
 			for (auto &ap : net->accessPoints){
 				cout<<" -- "<<apcount<<":  "<<ap->getDisplayableBSSID()<<"  "<<ap->getFrequency()<<"  "<<endl;
 				apcount++;
@@ -31,13 +31,13 @@ void test_connect(){
 					//connect to first access point
 					connect = true;
 					i->connect(ap);
-				}*/
+				}
 			}
 		}
 		i->disconnect();
-		cout<<"Disconnected!"<<endl;
+		cout<<"Disconnected!"<<endl;*/
 	}
-	int selectedInterface;
+	/*int selectedInterface;
 	int selectedNetwork;
 	int selectedAP;
 	cout<<"Selected interface to connect: ";
@@ -51,7 +51,7 @@ void test_connect(){
 	//connect to this ap
 	cout<<"Trying to connect to "<<networks[selectedNetwork]->accessPoints[selectedAP]->getDisplayableBSSID()<<" of "<<networks[selectedNetwork]->SSID<<" on "<<interfaces[selectedInterface]->getName()<<endl;
 	
-	interfaces[selectedInterface]->connect(networks[selectedNetwork]->accessPoints[selectedAP]);	
+	interfaces[selectedInterface]->connect(networks[selectedNetwork]->accessPoints[selectedAP]);*/
 
 }
 
