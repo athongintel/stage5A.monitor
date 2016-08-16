2315191de791b9ab24ee128e0f62d23fc7e2103d
tom <dangxuanthong@gmail.com>
2016-08-16 12:39:48 +0200

    - add daemon incoming and outgoing message processing threads - edit svc_command

--- wifi/svc/svc/SVC-daemon.cpp
+++ wifi/svc/svc/SVC-daemon.cpp
@@ -20,37 +20,49 @@ using namespace std;
 #define		SVC_DAEPORT			1221
 
 
-struct SVCCmd{
-	enum SVCCommand cmdID;
-	uint8_t argc;	
-};
-
-
 class DaemonService{
 	
 	public:	
 		string svcClientPath;
-		sockaddr_un sockAddress;
-		int sock;		
+		sockaddr_un unSockAddress;
+		int unSock;
+				
 		uint32_t appID;
+				
+		/*	one incomming and one outgoing queue */
+		MessageQueue* outgoingQueue;
+		MessageQueue* incomingQueue;
 		
-		/*	one incomming and one outgoing queue. already thread-safe (at least they said :D)*/
+		DaemonService(){
+			//init incoing and outgoing queue
+			this->appID = 0;
+			this->outgoingQueue = new MessageQueue();
+			this->incomingQueue = new MessageQueue();
+		}
 		
-		
-		DaemonService(uint32_t appID){
-			this->appID = appID;			
-			//create socket address
+		DaemonService(uint32_t appID) : DaemonService(){
+			this->appID = appID;
+				
+			//create unix socket and connect to send packet
 			this->svcClientPath = SVC_CLIENT_PATH_PREFIX + to_string(appID);
-			memset(&this->sockAddress, 0, sizeof(this->sockAddress));
-			this->sockAddress.sun_family = AF_LOCAL;
-			memcpy(this->sockAddress.sun_path, this->svcClientPath.c_str(), this->svcClientPath.size());			
-			//create new diagram socket and bind
-			this->sock = socket(AF_LOCAL, SOCK_DGRAM, 0);
-			connect(this->sock, (struct sockaddr*) &this->sockAddress, sizeof(this->sockAddress));
-			
-			/*	create new HTP socket, bind to 1221
-				for now using UDP socket, to be replaced
-			*/			
+			memset(&this->unSockAddress, 0, sizeof(this->unSockAddress));
+			this->unSockAddress.sun_family = AF_LOCAL;
+			memcpy(this->unSockAddress.sun_path, this->svcClientPath.c_str(), this->svcClientPath.size());					
+			this->unSock = socket(AF_LOCAL, SOCK_DGRAM, 0);
+			connect(this->unSock, (struct sockaddr*) &this->unSockAddress, sizeof(this->unSockAddress));
+		}
+		
+		int encryptMessage(const uint8_t* plainMessage, const size_t* plainLen, uint8_t* encryptedMessage, size_t* encryptedLen){
+		
+		}
+		
+		int decryptMessage(const uint8_t* encryptedMessage, const size_t* encryptedLen, uint8_t* plainMessage, size_t* plainLen){
+		
+		}
+	
+		~DaemonService(){
+			delete outgoingQueue;
+			delete incomingQueue;
 		}
 };
 
@@ -64,8 +76,13 @@ int daemonInSocket;
 
 pthread_t unixReadingThread;
 pthread_t htpReadingThread;
+pthread_t unixProcessingThread;
+pthread_t htpProcessingThread;
+
 pthread_attr_t htpReadingThreadAttr;
 pthread_attr_t unixReadingThreadAttr;
+pthread_attr_t unixProcessingThreadAttr;
+pthread_attr_t htpProcessingThreadAttr;
 
 string errorString;
 volatile bool working;
@@ -85,22 +102,98 @@ uint32_t appExisted(uint32_t appID){
 	return 0;
 }
 
-void processOutgoingCommand(const uint8_t* buffer, size_t len){
-	
-	//extract params
-	uint32_t sessionID = *((uint32_t*)receiveBuffer);			
-	//pass 4 byte session and 1 byte frame type
-	const uint8_t* pointer = receiveBuffer + 5;
-	
-	struct SVCCmd* cmd = (struct SVCCmd*)(pointer);
-	vector<SVCCommandParam*> argv;
 
-	//pass 2 byte of command type and argc
-	pointer+= 2;	
+void signal_handler(int sig){
+	if (sig == SIGINT){
+		printf("SIGINT caught, stopping daemon\n");
+		working = false;
+	}
+}
+
+void* unixReadingLoop(void* args){
 	
+	//forward packets to its correspond outgoing queue	
+	int byteRead;
+
+	while (working){	
+		do{
+			byteRead = recv(daemonSocket, receiveBuffer, SVC_DEFAULT_BUFSIZ, MSG_DONTWAIT);
+		}
+		while((byteRead==-1 && (errno==EAGAIN || errno==EWOULDBLOCK)) && working);		
+		
+		/*	process message. message is stored in receiveBuffer	*/
+		if (byteRead>0){			
+			printf("byte read from unix: %d\n", byteRead);
+			printBuffer(receiveBuffer, byteRead);
+			
+			/*	check if sessionID is registered	*/
+			uint32_t sessionID = *((uint32_t*)receiveBuffer);
+			printf("sessionID of this message: %08x\n", sessionID);
+			if (appTable[sessionID]!=NULL){			
+				appTable[sessionID]->outgoingQueue->enqueue(receiveBuffer, byteRead);			
+			}			
+		}
+		/*
+		else: read error
+		*/
+	}
+	cout<<"Exit unix reading loop"<<endl;
+}
+
+void* htpReadingLoop(void* args){
+	
+	/*	forward packet to correspondind incoming queue	*/
+	int byteRead;
+	
+	while (working){	
+		do{
+			byteRead = recv(daemonSocket, receiveBuffer, SVC_DEFAULT_BUFSIZ, MSG_DONTWAIT);
+		}
+		while((byteRead==-1 && (errno==EAGAIN || errno==EWOULDBLOCK)) && working);		
+		
+		/*	process message. message is stored in receiveBuffer	*/
+		if (byteRead>0){			
+			printf("byte read from htp: %d\n", byteRead);
+			printBuffer(receiveBuffer, byteRead);
+						
+			/*	check if sessionID is registered	*/
+			/*uint32_t sessionID = *((uint32_t*)receiveBuffer);
+			printf("sessionID of this message: %08x\n", sessionID);
+			if (appTable[sessionID]!=NULL){			
+				appTable[sessionID]->outgoingQueue->enqueue(receiveBuffer, byteRead);			
+			}*/			
+		}
+		/*
+		else: read error
+		*/
+	}
+	cout<<"Exit htp reading loop"<<endl;
+}
+
+void _processIncomingMessage(uint8_t* buffer, size_t len){
+	
+	/*	
+		The buffer passed into this function is stored in the message queue.
+		Hence, it is unnecessary to alloc memory as we can modify directly buffer.
+		Encrypted messages have already been decrypted.
+	*/
+	
+}
+
+void _processOutgoingMessage(uint8_t* buffer, size_t len){
+
+	/*	
+		The buffer passed into this function is stored in the message queue.
+		Hence, it is unnecessary to alloc memory as we can modify directly buffer.
+	*/
+	
+	vector<SVCCommandParam*> argv;
+	//pass 2 byte of command type and argc
+	pointer+= 2;
+
 	for (int i=0; i<cmd->argc; i++){
 		uint16_t length = *(pointer);
-		printf("param %d length: %04x, param: ",i, length);
+	//	printf("param %d length: %04x, param: ",i, length);
 	//	printBuffer(pointer+2, length);
 		argv.push_back(new SVCCommandParam(length, pointer+2));
 		//add 2 byte of length and the arg's length
@@ -113,11 +206,11 @@ void processOutgoingCommand(const uint8_t* buffer, size_t len){
 			uint32_t appID;
 			ssize_t sendre;
 			bool newAppAllowed;
-			
+
 			//a.1 check if app existed
 			appID = *((uint32_t*)(argv[0]->param));
 			//printf("appID: %08x\n",appID);
-			
+
 			newAppAllowed = false;
 			sessionID = appExisted(appID);
 			if (sessionID!=0){
@@ -128,7 +221,7 @@ void processOutgoingCommand(const uint8_t* buffer, size_t len){
 			else{
 				newAppAllowed = true;
 			}
-			
+
 			if (newAppAllowed){
 				//a.2 add new record to appTable
 				//create sessionID = hash(appID & rand)							
@@ -144,92 +237,118 @@ void processOutgoingCommand(const uint8_t* buffer, size_t len){
 
 		case SVC_CMD_CONNECT_STEP1:
 			printf("got connect step 1 command\n");
-			
+
 			break;
-			
+
 		default:
 			break;
 	}
 }
 
-void processOutgoingData(uint8_t* buffer, size_t len){
+
+void* htpProcessingLoop(void* args){
+	//iteration from all service and read from incoming queue
+	uint32_t sessionID;
+	DaemonService* service;
+	MessageQueue* queue;
+	uint8_t* buffer = (uint8_t*)malloc(SVC_DEFAULT_BUFSIZ);
+	uint8_t* decryptedMessage = (uint8_t*)malloc(SVC_DEFAULT_BUFSIZ);
+	size_t len;
+	size_t decryptedLen;
+	
+	while (working){	
+  		for (auto& it : appTable){  			
+    		service = it.second;
+    		queue = service->incomingQueue;
+    		
+    		if (queue->messageMutex.try_lock()){
+    			//dequeue the message
+    			if (queue->notEmpty()){
+    				if (queue->dequeue(buffer, &len)!=-1){
+    					/*	read for info	*/
+    					sessionID = it.first;
+						uint8_t infoByte = buffer[4];
+						if (infoByte & SVC_ENCRYPTED){							/
+							if (sessionID != SVC_DEFAUT_SESSIONID){
+								/*	decrypt the message	*/
+								if (service->decrypteMessage(buffer+5, len-5, decryptedMessage, &decryptedLen)){
+									/*	replace encrypted content with decrypted one and process */
+									memcpy(buffer+5, decryptedMessage, decryptedLen);
+									/*	update new size	*/
+									len = 5 + decryptedSize;
+									_processIncomingMessage(buffer, len);
+								}
+								/*
+								else: failed to decrypt message, ignore
+								*/							
+							}
+							/*
+							else: ignore encrypted packet with default sessionID
+							*/
+						}
+						else{
+							/*	non-encrypted packet	*/
+							if ((infoByte & SVC_COMMAND_FRAME)){
+								enum SVCCommand cmd = buffer[5];
+								if (!isEncryptedCommand(cmd)){
+									_processIncomingMessage(buffer, size);
+								}
+								/*
+								else: non-encrypted commands are only allowed for certain commands
+								*/
+							}
+							/*
+							else: non-encrypted data frame not allowed
+							*/							
+						}	
+    				}
+    			}
+    			queue->messageMutex.unlock();
+    		}
+    	}
+    }
+    
+    delete decryptedMessage;
+    delete buffer;
 }
 
 
-void processIncomingCommand(uint8_t* buffer, size_t len){
+void* unixProcessingLoop(void* args){
 	
-}
-
-void signal_handler(int sig){
-	if (sig == SIGINT){
-		printf("SIGINT caught, stopping daemon\n");
-		working = false;
-	}
-}
-
-void* unixReadingLoop(void* args){
-
-	//string test;
-	//read message from socket in blocking-mode
+	DaemonService* service;
+	MessageQueue* queue;
 	
-	struct sigaction act;
-	act.sa_handler = signal_handler;
-	sigfillset(&act.sa_mask);
-	sigdelset(&act.sa_mask, SIGINT);
-	sigaction(SIGINT, &act, NULL);
-	int byteRead;
-
-	while (working){
+	uint8_t* buffer = (uint8_t*)malloc(SVC_DEFAULT_BUFSIZ);
+	size_t len;
 	
-		do{
-			byteRead = recv(daemonSocket, receiveBuffer, SVC_DEFAULT_BUFSIZ, MSG_DONTWAIT);
-		}
-		while((byteRead==-1 && (errno==EAGAIN || errno==EWOULDBLOCK)) && working);		
+	/*	read from service outgoing queue	*/
+	while(working){
+		for (auto& it : appTable){				
+			service = it.second;
+			queue = service->outgoingQueue;
 			
-		//printf("byte read: %d, errno: %d, working: %d\n", byteRead, errno, working);
-		/*	process message. message is stored in receiveBuffer	*/
-		if (byteRead>0){
-			//printf("read a message of %d\n", (int)byteRead);
-			printf("byte read: %d\n", byteRead);
-			printBuffer(receiveBuffer, byteRead);
-			
-			/*	check if sessionID is registered	*/
-			uint32_t sessionID = *((uint32_t*)receiveBuffer);
-			//printf("sessionID of this message: %08x\n", sessionID);
-			if (sessionID==0){				
-				if (receiveBuffer[4]==SVC_COMMAND_FRAME && receiveBuffer[5]==SVC_CMD_REGISTER_APP){
-					processOutgoingCommand(receiveBuffer, byteRead);
-				}
-				/*
-					else: frame error, ignore. sessionID = 0 only allowed for SVC_CMD_REGISTER_APP
-				*/
-			}
-			else{
-				if (appTable[sessionID]!=NULL){
-					if (receiveBuffer[4]==SVC_COMMAND_FRAME){
-						processOutgoingCommand(receiveBuffer, byteRead);
-					}
-					else if (receiveBuffer[4]==SVC_DATA_FRAME) {
-						processOutgoingData(receiveBuffer, byteRead);
+			/*	dequeue message	*/
+			if (queue->messageMutex.try_lock()){			
+				if (queue->notEmpty()){
+					if (queue->dequeue(buffer, &size)!=-1){
+						/*	process message	*/
+						_processOutgoingMessage(buffer, size);
 					}
 					/*
-						else: invalid frame format, justs ignore
+					else: dequeue failed
 					*/
 				}
 				/*
-					else: sessionID not existed
+				else: queue is empty
 				*/
 			}
+			/*
+			else: cannot lock mutex
+			*/
 		}
-		/*
-			else: read error
-		*/
 	}
-	cout<<"Exit reading loop"<<endl;	
-}
-
-void* htpReadingLoop(void* args){
 	
+	delete buffer;
 }
 
 
@@ -238,9 +357,8 @@ int main(int argc, char** argv){
 	receiveBuffer = (uint8_t*)malloc(SVC_DEFAULT_BUFSIZ);	
 	
 	//1. check if the daemon is already existed
-	int unlinkre = unlink(SVC_DAEMON_PATH.c_str());
-	if (!(unlinkre==0 || (unlinkre==-1 && errno==ENOENT))){
-		cout<<"unlink rs "<<unlinkre<<", error: "<<errno<<endl;
+	int rs = unlink(SVC_DAEMON_PATH.c_str());
+	if (!(rs==0 || (rs==-1 && errno==ENOENT))){	
 		errorString = SVC_ERROR_NAME_EXISTED;
 		goto errorInit;
 	}
@@ -275,6 +393,9 @@ int main(int argc, char** argv){
 	sigfillset(&act.sa_mask);
 	sigdelset(&act.sa_mask, SIGINT);
 	sigaction(SIGINT, &act, NULL);
+	
+	//add default DaemonService to handle non-encrypted request
+	appTable[SVC_DEFAULT_SESSIONID] = new DaemonService();
     
     //4.	create a thread to read from unix domain socket
     working = true;
@@ -284,7 +405,15 @@ int main(int argc, char** argv){
 	//5.	create a thread to read from htp socket
 	pthread_attr_init(&htpReadingThreadAttr);	
 	pthread_create(&htpReadingThread, &htpReadingThreadAttr, htpReadingLoop, NULL);
-   
+	
+	//6.	create a thread to process and redirect incoming messages
+	pthread_attr_init(&htpProcessingThreadAttr);	
+	pthread_create(&htpProcessingThread, &htpProcessingThreadAttr, htpProcessingLoop, NULL);
+
+	//7.	create a thread to process and redirect outgoing messages
+   	pthread_attr_init(&unixProcessingThreadAttr);	
+	pthread_create(&unixProcessingThread, &unixProcessingThreadAttr, unixProcessingLoop, NULL);
+
     goto initSuccess;
     
     errorInit:
@@ -295,12 +424,13 @@ int main(int argc, char** argv){
     	//do something
     	printf("SVC daemon is running...\n");
     	pthread_join(unixReadingThread, NULL);   
-    	
+    	pthread_join(unixProcessingThread, NULL);
+    	pthread_join(htpReadingThread, NULL);
+    	pthread_join(htpProcessingThread, NULL);    	
     	printf("SVC daemon stopped.\n");
     	
     	//do cleaning up
-    	unlink(SVC_DAEMON_PATH.c_str());
-    	
+    	unlink(SVC_DAEMON_PATH.c_str());    	
 }
 
 
